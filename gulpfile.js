ad7bf4b8dc28a022754339546543154c0a93b056
Shirudzaki <shihimitso@gmail.com>
2021-10-15 19:55:31 +0300

    complete gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,182 @@
+// const fileinclude = require('gulp-file-include');
+
+let project_folder = "dist"; // финальная папка для заказчика
+let source_folder = "#src"; // папка с исходным кодом в которой мы работаем
+
+let fs = require("fs");
+
+let path = {
+    //пути выгрузок файлов
+    build: {
+        html: project_folder + "/",
+        css: project_folder + "/css/",
+        js: project_folder + "/js/",
+        img: project_folder + "/img/",
+        fonts: project_folder + "/fonts/",
+    },
+    src: {
+        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],  //исключаем файлы с подчеркиванием
+        css: source_folder + "/scss/style.scss", //gulp будет обрабатывать не все файлы а только 1 который будет собирать в себе все подключенные файлы
+        js: source_folder + "/js/script.js", //фналогично с css-scss выше
+        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}", //слушать все папки и подпапки картинок и только такие расширения
+        fonts: source_folder + "/fonts/*.ttf", //только ttf
+    },
+    //пути к файлам которые нужно слушать постоянно (изменения в них) и сразу что-то выполнять
+    watch: {
+        html: source_folder + "/**/*.html", //слушаем всё что html
+        css: source_folder + "/scss/**/*.scss",
+        js: source_folder + "/js/**/*.js",
+        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    },
+    clean: "./" + project_folder + "/"   //путь для удаления папки каждый раз при запуске галпа
+
+}
+
+
+//переменные для плагинов и дополнений 
+let { src, dest, tree } = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require('browser-sync').create(),
+    fileinclude = require("gulp-file-include"),
+    del = require("del"),
+    scss = require("gulp-sass")(require('sass')),
+    autoprefixer = require("gulp-autoprefixer"),
+    group_media = require("gulp-group-css-media-queries"),
+    clean_css = require("gulp-clean-css"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2"),
+    fonter = require("gulp-fonter");
+
+
+//запукаем сервер и плагин.
+function browserSync(params) {
+    browsersync.init({
+        server: {
+            baseDir: "./" + project_folder + "/"
+        },
+        port: 3000,
+        notify: false //выключает кастомную табличку "Браузер обновился", чтобы не бесила!
+    })
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(group_media())
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 5 versions"],
+                cascade: true
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(clean_css())
+        .pipe(
+            rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
+}
+
+function js() {
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+}
+
+function images() {
+    return src(path.src.img)
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
+
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts))
+}
+
+gulp.task('otf2ttf', function () {
+    return src([source_folder + '/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(source_folder + '/fonts/'))
+})
+
+function fontsStyle(params) {
+
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {
+
+}
+
+function watchFiles(params) {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
